d5341563a71f60bfdc1e7b4d4eb01d1d4f79c512
Maheshwar Jayaraman <maheshwar_eng@hotmail.com>
2011-11-12 20:51:14 -0800

    Commiting .gitignore changes

--- LeaderboardSDK/Model/GetLeaderboardRequest.cs
+++ LeaderboardSDK/Model/GetLeaderboardRequest.cs
@@ -5,5 +5,7 @@ namespace Anishoo.Services.Leaderboard.Model
     [DataContract]
     public class GetLeaderboardRequest : LeaderboardRequest
     {
+        [DataMember(Name = "page")]
+        public int StartPage { get; set; }
     }
 }
--- LeaderboardSDK/Model/LeaderboardRequest.cs
+++ LeaderboardSDK/Model/LeaderboardRequest.cs
@@ -5,7 +5,5 @@ namespace Anishoo.Services.Leaderboard.Model
     [DataContract]
     public abstract class LeaderboardRequest
     {
-        [DataMember(Name="page")]
-        public int StartPage { get; set; }
     }
 }
